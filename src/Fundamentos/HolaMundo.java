07b90f904095c24a7d097b47bcc01cd752576a86
Sebastian Franco <francosebastian802@gmail.com>
2024-09-22 00:19:51 -0500

    Update

--- src/Fundamentos/HolaMundo.java
+++ src/Fundamentos/HolaMundo.java
@@ -1,7 +1,9 @@
 package Fundamentos;
 
 public class HolaMundo {
+    // El método main es el punto de entrada del programa.
     public static void main(String[] args) {
+        // Imprime Hola Mundo en la consola.
         System.out.println("Hola Mundo");
     }
 }
