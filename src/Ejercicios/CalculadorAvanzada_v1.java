dfdc0420c49c0a5d40bb1d9e353b3274f5ddc0f7
Sebastian Franco <48168306+SAFCCFAS@users.noreply.github.com>
2024-10-16 04:20:47 +0000

    agregar CalculadoraAvanzada_v1.java

---
+++ src/Ejercicios/CalculadorAvanzada_v1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class CalculadorAvanzada_v1{
+    static Scanner leer = new Scanner(System.in); //variable estatica
+    public static void main(String[] args) {
+        double suma,resta,multiplicacion, modulo, division, operacion, potencia, raiz;
+        
+    }
+}
