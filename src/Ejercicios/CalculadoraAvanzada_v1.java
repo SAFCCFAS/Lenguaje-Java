eef8423795f4e1ac1670931786ccec668be848a2
Sebastian Franco <francosebastian802@gmail.com>
2024-10-16 22:44:36 -0500

    actualizar CalculadoraAvanzada_v1.java

--- src/Ejercicios/CalculadoraAvanzada_v1.java
+++ src/Ejercicios/CalculadoraAvanzada_v1.java
@@ -6,36 +6,36 @@ public class CalculadoraAvanzada_v1 {
     static Scanner leer = new Scanner(System.in); // variable estatica
 
     public static void main(String[] args) {
-        double n1, n2, suma, resta, multiplicacion, division, modulo, potencia, raiz; //declaración de variables
+        double n1, n2, suma, resta, multiplicacion, division, modulo, potencia, raiz; // declaración de variables
         int operacion, resultado;
-        String nombreoperacion = null;
+        String nombreoperacion= null;
+        boolean control=true;
+        do {
+            System.out.println("Bienvenido(a)!, ¿Que operación desea realizar?" +
+                    "\n1.Suma.\n2.Resta.\n3.Multiplicación.\n4.División.\n5.Modulo.\n6.Potencia.\n7.Raiz Cuadrada." +
+                    "\n8.Salir.");
+            operacion = leer.nextInt(); // inicialización de las variables
+            if (operacion >= 1 && operacion <= 4) {
+                switch (operacion) {
+                    case 1:
+                        nombreoperacion = "Suma";
+                        break;
+                    case 2:
+                        nombreoperacion = "Resta";
+                        break;
+                    case 3:
+                        nombreoperacion = "Multiplicación";
+                        break;
+                    case 4:
+                        nombreoperacion = "División";
+                        break;
+                    default:
+                        System.out.println("Error: opción o ingreso, válido intente nuevamente.");
+                }
+                System.out.println("Usted a elegido: " + nombreoperacion);
 
-
-        System.out.println("Bienvenido(a)!, ¿Que operación desea realizar?" +
-                "\n1.Suma.\n2.Resta.\n3.Multiplicación.\n4.División.\n5.Modulo.\n6.Potencia.\n7.Raiz Cuadrada." +
-                "\n8.Salir.");
-        operacion = leer.nextInt(); // inicialización de las variables
-        if (operacion == 1) {
-            nombreoperacion = "Suma"; //asignación de variable
-        }
-        System.out.println("Usted a elegido: " + nombreoperacion);
-        System.out.println("Ingrese el primer número");
-        n1 = leer.nextDouble();
-        System.out.println("Ingrese el segundo número");
-        n2 = leer.nextDouble();
-        switch (operacion) {
-            case 1:
-                suma = n1 + n2;
-                resultado = (int) suma;
-                System.out.println("El resultado de la " + nombreoperacion + " es " + resultado + ".");
-                break;
-            case 2:
-                resta = n1 - n2;
-                resultado = (int) resta;
-                System.out.println("El resultado de la " + nombreoperacion + " es " + resultado + ".");
-                break;
-            case 3:
-                
-        }
+                control=false;
+            }
+        }while (control);
     }
 }
