98f8a51bc453af8e755a70a7afb5ea7ab3201e2d
Sebastian Franco <francosebastian802@gmail.com>
2024-11-18 19:53:10 -0500

    remove

--- src/Fundamentos/HolaMundo.java
+++
@@ -1,9 +0,0 @@
-package Fundamentos;
-
-public class HolaMundo {
-    // El método main es el punto de entrada del programa.
-    public static void main(String[] args) {
-        // Imprime Hola Mundo en la consola.
-        System.out.println("¡Hola Mundo!");
-    }
-}
--- src/Fundamentos/Variables.java
+++
@@ -1,9 +0,0 @@
-package Fundamentos;
-
-public class Variables {
-    public static void main(String[] args) {
-        int edad = 28; // Declaración de variables.
-        String nombre = "Sebastian";
-        System.out.println("Hola " + nombre + " ahora tienes " + edad);
-    }
-}
