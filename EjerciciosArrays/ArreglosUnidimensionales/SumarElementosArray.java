ad4783833b8716c58a5cbfac965fa548a7c7d6b8
Sebastian Franco <franosebastian802@gmail.com>
2024-11-27 21:23:37 -0500

    chore: actualizar SumarElementosArray.java

--- EjerciciosArrays/ArreglosUnidimensionales/SumarElementosArray.java
+++ EjerciciosArrays/ArreglosUnidimensionales/SumarElementosArray.java
@@ -8,19 +8,25 @@ public class SumarElementosArray {
         int cantidad, n, suma = 0;
         int[] tam;
 
-        System.out.println("Bienvenido(a), por favor ingrese la cantidad de números que desea almacenar en el arreglo:");
-        cantidad = sc.nextInt();
+        do {
+            System.out.println("Bienvenido(a), por favor ingrese la cantidad de números que desea almacenar en el arreglo:");
+            cantidad = sc.nextInt();
+            if (cantidad == 0) {
+                System.err.println("Error: La cantidad debe ser mayor a 0, intente nuevamente.");
+            }
+        } while (cantidad <= 0);
 
-        tam = new int[cantidad];
+        tam = new int[cantidad]; // Crear el arreglo de acuerdo al tamaño
 
+        // Leer los elementos del arreglo
         for (int i = 0; i < tam.length; i++) {
             System.out.println("Ingrese el número de la posición " + i + ":");
             tam[i] = sc.nextInt();
         }
 
+        // Sumar los elementos del arreglo
         for (int i = 0; i < cantidad; i++) {
             suma += tam[i];
-
         }
 
         System.out.println("La suma total de los números ingresados es: " + suma);
