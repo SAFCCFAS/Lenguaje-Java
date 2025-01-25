5c374215dc9ce27577204bb46eb998df56b74dc9
Sebastian Franco <franosebastian802@gmail.com>
2025-01-25 00:06:10 -0500

    refactor

---
+++ EjerciciosArrays/ArreglosUnidimensionales/TablaDeMultiplicarArray.java
@@ -0,0 +1,33 @@
+package EjerciciosArrays.ArreglosUnidimensionales;
+
+import java.util.Scanner;
+
+public class TablaDeMultiplicarArray {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		String entrada;
+		int[] resultados = new int[12];
+		int contar, valor;
+		boolean ingresoValido;
+
+		while (true) {
+			System.out.println("Bienvenido(a), favor ingrese número a mostrar tabla de operación");
+			entrada = sc.nextLine().trim();
+			ingresoValido = !entrada.trim().isEmpty() && entrada.matches("\\d+");
+			if (!ingresoValido || Integer.parseInt(entrada) <= 0) {
+				System.err.println("Error: Entrada no válida. Ingrese un número mayor que 0.");
+				continue;
+			}
+			valor = Integer.parseInt(entrada);
+
+			for (contar = 0; contar < resultados.length; contar++) {
+				resultados[contar] = valor * contar + 1;
+				System.out.printf("%d x %d: %d\n", valor, contar + 1, resultados[contar]);
+			}
+
+			break;
+		}
+	}
+
+}
