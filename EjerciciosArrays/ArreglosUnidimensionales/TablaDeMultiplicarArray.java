225401fb21d409a140523d46238a1a4a93d39f68
Sebastian Franco <francosebastian802@gmail.com>
2025-01-26 12:19:54 -0500

    feat: agregar clase TablaDeMultiplicarArray para la generación de tablas de multiplicar

---
+++ EjerciciosArrays/ArreglosUnidimensionales/TablaDeMultiplicarArray.java
@@ -0,0 +1,10 @@
+package EjerciciosArrays.ArreglosUnidimensionales;
+
+public class TablaDeMultiplicarArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
